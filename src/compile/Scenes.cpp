9316d22376ad0909e2b4e54779cc46901c16f42d
Hnimdantoc <88573122+Hnimdantoc@users.noreply.github.com>
2023-03-31 22:15:05 +0700

    Delete src directory

--- src/compile/Scenes.cpp
+++
@@ -1,4 +0,0 @@
-#include "Scenes.hpp"
-
-Scene_0* Scene_0::static_instance = nullptr;
-Menu* Menu::static_instance = nullptr;
--- src/compile/Timer.cpp
+++
@@ -1,5 +0,0 @@
-#include "Timer.hpp"
-
-Timer* Timer::static_Instance = nullptr;
-
-Timer::Timer(){}
